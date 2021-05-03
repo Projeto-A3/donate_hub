975ad287a5726671cf16a21d9a5b9a191d1c393b
EduM574 <eduardomoraeslima@hotmail.com>
2021-05-02 23:10:36 -0300

    definicao banco de dados

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import UserController from '@controllers/UserController';
+import authMiddleware from '@middlewares/authMiddleware';
 
 const routes = Router();
 
 routes.post('/user', UserController.store);
 routes.post('/auth', UserController.authenticate);
+routes.get('/users', authMiddleware, UserController.index);
 
 export default routes;
